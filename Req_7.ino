5aa91172489ad3239d9b5c56886d1cb0374e8363
khpark17 <103479646+khpark17@users.noreply.github.com>
2022-04-18 11:18:56 +0900

    Update Req_7.ino

--- Req_7.ino
+++ Req_7.ino
@@ -6,44 +6,44 @@ void setup() {
 
 void loop() {
   // Open : 131Hz 1번 부저음 발생
-  tone(buzzerPin, 131); // 131 Hz
+  tone(buzzerPin, 131, 500); // 131 Hz
   delay(1000); // 1 sec delay
   noTone(buzzerPin); // OFF
   delay(1000); // 1 sec delay
 
   // Close : 262Hz 2번 부저음 발생
-  tone(buzzerPin, 262);
+  tone(buzzerPin, 262, 500);
   delay(1000);
   noTone(buzzerPin);
   delay(1000);
   
-  tone(buzzerPin, 262);
+  tone(buzzerPin, 262, 500);
   delay(1000);
   noTone(buzzerPin);
   delay(1000);
 
   // Safety Mode : 손이 감지되면 300Hz 빠르게 5번 부저음 발생
-  tone(buzzerPin, 300);
+  tone(buzzerPin, 300, 500);
   delay(300);
   noTone(buzzerPin);
   delay(300);
 
-  tone(buzzerPin, 300);
+  tone(buzzerPin, 300, 500);
   delay(300);
   noTone(buzzerPin);
   delay(300);
 
-  tone(buzzerPin, 300);
+  tone(buzzerPin, 300, 500);
   delay(300);
   noTone(buzzerPin);
   delay(300);
 
-  tone(buzzerPin, 300);
+  tone(buzzerPin, 300, 500);
   delay(300);
   noTone(buzzerPin);
   delay(300);
 
-  tone(buzzerPin, 300);
+  tone(buzzerPin, 300, 500);
   delay(300);
   noTone(buzzerPin);
   delay(300);
