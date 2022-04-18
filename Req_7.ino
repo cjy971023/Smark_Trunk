4422b00c26a754d761dbc92fb84e289192de22c9
khpark17 <103479646+khpark17@users.noreply.github.com>
2022-04-18 09:33:03 +0900

    Add files via upload

---
+++ Req_7.ino
@@ -0,0 +1,50 @@
+int buzzerPin = 2;
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  // Open : 131Hz 1번 부저음 발생
+  tone(buzzerPin, 131); // 131 Hz
+  delay(1000); // 1 sec delay
+  noTone(buzzerPin); // OFF
+  delay(1000); // 1 sec delay
+
+  // Close : 262Hz 2번 부저음 발생
+  tone(buzzerPin, 262);
+  delay(1000);
+  noTone(buzzerPin);
+  delay(1000);
+  
+  tone(buzzerPin, 262);
+  delay(1000);
+  noTone(buzzerPin);
+  delay(1000);
+
+  // Safety Mode : 손이 감지되면 300Hz 빠르게 5번 부저음 발생
+  tone(buzzerPin, 300);
+  delay(300);
+  noTone(buzzerPin);
+  delay(300);
+
+  tone(buzzerPin, 300);
+  delay(300);
+  noTone(buzzerPin);
+  delay(300);
+
+  tone(buzzerPin, 300);
+  delay(300);
+  noTone(buzzerPin);
+  delay(300);
+
+  tone(buzzerPin, 300);
+  delay(300);
+  noTone(buzzerPin);
+  delay(300);
+
+  tone(buzzerPin, 300);
+  delay(300);
+  noTone(buzzerPin);
+  delay(300);
+}
